eda77830e494e04172fad5e77c4e93f936bf0b8e
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-09-08 11:23:14 +0530

    Learned how handle errors

--- prod/seasons/src/index.js
+++ prod/seasons/src/index.js
@@ -7,22 +7,28 @@ class App extends React.Component {
         super(props);
 
         // THIS IS THE ONLY TIME we do assignment to this.state
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: "" };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // we called setState!!!
                 this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
         );
     }
 
     // React says we have to define render!!
     render() {
         return(
-            <div>Latitude: {this.state.lat}</div>
-        )
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
 }
 
